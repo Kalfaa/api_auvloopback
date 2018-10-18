e4c94ba1d4af8141dd054b5922b5918120e0fea9
Kalfa <theo92320@hotmail.fr>
2018-10-18 10:14:50 +0200

    script db

---
+++ server/boot/autoupgrade.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function enableAuthentication(server) {
+  const ds = server.dataSources.db;
+
+  const models = [
+    'ACL',
+    'AccessToken',
+    'profile',
+  ];
+
+  ds.setMaxListeners(Infinity);
+
+  ds.autoupdate(models, function(err) {
+    if (err) throw err;
+    console.log('Tables [' + models + '] created in ', ds.adapter.name);
+  });
+};
